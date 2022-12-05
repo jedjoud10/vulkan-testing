7cffd272cca0f48fb71796b0a5e6e6afcb50328f
jedjoud10 <jribijed6@gmail.com>
2022-12-05 11:20:22 -0500

    Add defaults

---
+++ src/instance.rs
@@ -0,0 +1,4 @@
+mod debug;
+mod instance;
+pub(super) use debug::*;
+pub use instance::*;
---
+++ src/queues.rs
@@ -0,0 +1,8 @@
+mod family;
+mod pool;
+mod queue;
+mod queues;
+pub use family::*;
+pub use pool::*;
+pub use queue::*;
+pub use queues::*;
