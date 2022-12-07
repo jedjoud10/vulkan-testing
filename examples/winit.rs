db9757cfc46d55d1c66a910add8078a0de49145d
jedjoud10 <jribijed6@gmail.com>
2022-12-07 17:03:20 -0500

    It works, I just have to make the API a lot cleaner now

--- examples/winit.rs
+++ examples/winit.rs
@@ -1,3 +1,5 @@
+use ash::vk;
+use gpu_allocator::MemoryLocation;
 use winit::{
     event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
@@ -20,30 +22,44 @@ fn main() {
         engine_name,
     ) };
 
-    // Create a surface that we shall render to
-    let surface = unsafe { Surface::new(&instance, &window) };
+    unsafe {
+        // Setup the vulkan state
+        let surface = Surface::new(&instance, &window);
+        let adapter = Adapter::pick(&instance, &surface);
+        let device = Device::new(&instance, &adapter);
+        let queue = Queue::new(&instance, &device, &adapter);
+        let swapchain = Swapchain::new(&adapter, &surface, &device, &instance, &window, false);
+        
+        // Create two buffers
+        let (buffer1, mut alloc1) = device.create_buffer(4, vk::BufferUsageFlags::TRANSFER_SRC, MemoryLocation::CpuToGpu, &queue);
+        let ptr = alloc1.mapped_slice_mut().unwrap();
+        ptr.copy_from_slice(&[1, 2, 3, 4]);
+        log::debug!("{:?}", ptr);
+        let (buffer2, alloc2) = device.create_buffer(4, vk::BufferUsageFlags::TRANSFER_DST, MemoryLocation::GpuOnly, &queue);
 
-    // Pick a physical device (adapter)
-    let adapter = unsafe { Adapter::pick(&instance, &surface) };
-
-    // Create a new device with those queues
-    let device = unsafe { Device::new(&instance, &adapter) };
-
-    // Create the queues that we will submit to
-    //let queues = unsafe { Queues::new(&instance, &device, &adapter) };
-
-    // Being the event loop
-    event_loop.run(move |event, _, control_flow| {
-        *control_flow = ControlFlow::Wait;
-
-        match event {
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                window_id,
-            } if window_id == window.id() => *control_flow = ControlFlow::Exit,
-            Event::MainEventsCleared => {
-            },
-            _ => (),
-        }
-    });
+        // Aquire a new recorder
+        let mut recorder = queue.aquire(&device, false, false);
+        let copy = vk::BufferCopy::builder().size(4).dst_offset(0).src_offset(0);
+        recorder.copy_buffer(buffer1, buffer2, vec![*copy]);
+        let submission = queue.submit(&device, recorder);
+        
+        // Destroy the buffers
+        device.destroy_buffer(buffer1, alloc1);
+        device.destroy_buffer(buffer2, alloc2);
+        
+        // Being the event loop
+        event_loop.run(move |event, _, control_flow| {
+            *control_flow = ControlFlow::Wait;
+            
+            match event {
+                Event::WindowEvent {
+                    event: WindowEvent::CloseRequested,
+                    window_id,
+                } if window_id == window.id() => *control_flow = ControlFlow::Exit,
+                Event::MainEventsCleared => {
+                },
+                _ => (),
+            }
+        });
+    }
 }
